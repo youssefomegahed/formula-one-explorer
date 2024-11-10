775fbc801eea36126ebac3f6e8b8b21b837dc6f5
Youssef Megahed <64082427+youssefomegahed@users.noreply.github.com>
2024-11-10 19:27:17 +0200

    made full season card clickable

--- src/components/SeasonCard.tsx
+++ src/components/SeasonCard.tsx
@@ -8,16 +8,16 @@ type SeasonCardProps = {
 
 export default function SeasonCard({ season, isCardView }: SeasonCardProps) {
     return (
-        <div
-            className={`${
-                isCardView
-                    ? 'bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition'
-                    : 'border-b border-gray-200 py-4'
-            }`}>
-            <Link href={`/seasons/${season.season}`} className="text-blue-600 hover:underline">
-                <h2 className="text-xl font-bold">{season.season}</h2>
-            </Link>
-            {isCardView && <p className="mt-2 text-gray-500">Explore the {season.season} season details.</p>}
-        </div>
+        <Link href={`/seasons/${season.season}`} className="block">
+            <div
+                className={`${
+                    isCardView
+                        ? 'bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition'
+                        : 'border-b border-gray-200 py-4'
+                }`}>
+                <h2 className="text-xl font-bold text-blue-600 hover:underline">{season.season}</h2>
+                {isCardView && <p className="mt-2 text-gray-500">Explore the {season.season} season details.</p>}
+            </div>
+        </Link>
     );
 }
